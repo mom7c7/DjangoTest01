348cc4055d49c352d74bfead8331c18b466877f0
m.narimani <mohammad77narimani@gmail.com>
2025-02-25 12:08:00 +0330

    Delete Create Action

--- projTest/appTest/views.py
+++ projTest/appTest/views.py
@@ -14,28 +14,6 @@ def home(request):
     }
     return HttpResponse(template.render(context, request))
 
-def create(request):
-    template = loader.get_template('createperson.html')
-    return HttpResponse(template.render())
-
-def createAction(request,p1 : Person):
-    p1 = Person(
-        firstname=p1.firstname,
-        lastname=p1.lastname,
-        email=p1.email,
-        age=p1.age,
-        username=p1.username,
-        password=p1.password,
-    )
-    p1.save()
-    template = loader.get_template('home.html')
-    persons = Person.objects.all().values('id','firstname')
-    context = {
-        'persons': persons
-    }
-    return HttpResponse(template.render(context, request))
-
-
 def details(request,id):
     template = loader.get_template('details.html')
     persons = Person.objects.get(id=id)
